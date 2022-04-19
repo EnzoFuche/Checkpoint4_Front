60cfb3f0fe98a6ccf996bdba6541f7460adcc1b3
EnzoFuche <fuche.enzo@gmail.com>
2022-04-19 12:03:44 +0200

    fini en presque

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Login from "./Components/login/login";
+import Accueil from "./Components/Accueil/Acceuil";
 
 function App() {
   return (
     <div className="App">
-      <h1>Coucou!</h1>
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
